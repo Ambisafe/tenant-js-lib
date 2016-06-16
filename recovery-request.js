9d3226c630d098892c85f6ddb2148a2c22c8d48c
Oleksii Matiiasevych <lastperson@gmail.com>
2016-06-16 19:07:45 +0530

    Remove unnecessary params from exposed functions.

--- recovery-request.js
+++ recovery-request.js
@@ -1,4 +1,4 @@
 var AmbiTenant = require('./index');
-var ambiTenant = new AmbiTenant(process.argv[2], process.argv[3]);
-console.log(JSON.stringify(ambiTenant.recoveryRequest(process.argv[4], process.argv[5], process.argv[6])));
+var ambiTenant = new AmbiTenant();
+console.log(JSON.stringify(ambiTenant.recoveryRequest(process.argv[2], process.argv[3], process.argv[4])));
 process.exit(0);
